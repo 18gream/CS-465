0aabe4398b5f48a6a02fc327bb9e941beb40a070
Gracie Ream <gracieream16@gmail.com>
2025-05-25 14:25:42 -0400

    Module 3 Completed Baseline

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var travelRouter = require('./app_server/routes/travel');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 // register handlebar partials (https://www.npmjs.com/package/hbs)
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+
+var trips = JSON.parse(fs.readFileSync(path.join(__dirname,'../../data/trips.json'),'utf8'))
+
 /* GET Travel View */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
